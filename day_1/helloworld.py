d0ea2c376d1abdfc2745d00a3c830bae4b91e9cf
KanticField <clementdeville3@gmail.com>
2023-09-12 11:33:40 +0200

    Adding Day 1

---
+++ day_1/helloworld.py
@@ -0,0 +1,28 @@
+#1
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 + 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+#2
+
+print('Clement')
+print('DEVILLE')
+print('France')
+print('I am Enjoying 30 days of Python')
+
+#3
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Clement'))
+print(type('DEVILLE'))
+print(type('FRANCE'))
---
+++ day_1/level3.py
@@ -0,0 +1,20 @@
+#numbers
+
+print(15)
+print(15.12)
+print(15 - 1j)
+
+#Others
+
+str = 'Yo'
+my_boolean = True
+my_list = ['Clement', 'DEVILLE']
+my_tuple = (3, 5, 7)
+my_set = {9, 1, 9}
+my_dict = {"10": "dix", "100": "cent", "1000": "mille"}
+
+#Euclidian Distance
+
+from math import *
+
+print(sqrt((2 - 10) ** 2 + (3 - 8) ** 2))
